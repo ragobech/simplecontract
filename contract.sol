413f62f922b86002049d1832bc16a1519da0d7c4
ragobech <chris.ragobeer@gmail.com>
2022-03-06 09:38:17 -0500

    formatting

--- contract.sol
+++ contract.sol
@@ -10,24 +10,18 @@ contract SendMoneyContract{
  event moneySent(address _from, address _to, uint _amount);
  
  constructor public {
- 
     owner = msg.sender;
     moneyBalance[msg.sender] = 50;
- 
  }
  
  
  function sendMoney(address _to, uint _amount) public returns(bool) {
-    require(msg.sender = owner; "You are not allowe");
+    require(msg.sender = owner; "You are not allowed");
     require(Balance[msg.sender] >= _amount; "Not enough money");
  
     moneyBalance[msg.sender] -= _amount;
     moneyBalance[_to] += _amount;
  
     emit moneySent(msg.sender, _to, _amount);
- 
- 
     return true;
- 
- 
  }
